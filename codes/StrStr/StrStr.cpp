2080e501e63d6f442185331c8780fb69e9e2ed78
FrankXu7 <1248456937@qq.com>
2021-04-01 11:37:27 +0800

    完成字符串搜索算法StrStr

--- codes/StrStr/StrStr.cpp
+++ codes/StrStr/StrStr.cpp
@@ -1,3 +1,23 @@
+/**************************************************************************************************
+ * 【题目描述】
+ * 搜索字符串中是否包含某个子字符串，返回其下标；
+ * 当子字符串为空串，返回下标0；
+ * 当子字符串长度大于搜索字符串长度，返回-1；
+ *
+ * 【输入】
+ * haystack="1266634", needle="666"
+ * 【输出】
+ * 是否有效括号配对
+ * index=2
+ *
+ * 【解题思路】
+ * 遍历搜索字符串，如果发现某个字符和子字符串首字符相同，则标识开始匹配，当字符串搜索完后，即得到结果；
+ * 如果匹配搜索发现有字符不相等，需要重置搜索数据（重置记录的搜索起始下标和搜索下标）；
+ * 特殊情况：当首字符相等，开始匹配时，如果子字符串长度，大于搜索字符串剩余未比较字符数，返回-1，标识未匹配到。
+ *
+ * @author FrankX
+ * @date 2021-04-01
+ **************************************************************************************************/
 #include <iostream>
 #include <string>
 
@@ -6,23 +26,29 @@ using namespace std;
 int StrStr(string& haystack, string& needle)
 {
 	if (needle.length() == 0) return 0;
+	else if (needle.length() > haystack.length()) return -1;
 
 	int strIdx = -1;
-	int searchIdx = 0;
+	unsigned int searchIdx = 0;
 
-	for (int idx = 0; idx < haystack.length(); ++idx)
+	for (unsigned int idx = 0; idx < haystack.length(); ++idx)
 	{
-		if (searchIdx >= needle.length())
+		if (haystack[idx] == needle[searchIdx])
 		{
-			break;
-		}
-		else if (haystack[idx] == needle[idx])
-		{
-			++strIdx;
+			if (strIdx == -1)
+			{
+				// 开始匹配时，子字符串大于目标字符串剩余未匹配字符数，标识为未匹配到
+				if (haystack.length() - idx < needle.length() - searchIdx) break;
+				strIdx = idx;
+			}
 			++searchIdx;
+
+			// 子字符串搜索完成，且匹配到
+			if (searchIdx >= needle.length()) break;
 		}
-		else
+		else if (searchIdx > 0)
 		{
+			// 之前的匹配不成功，重置搜索数据
 			strIdx = -1;
 			searchIdx = 0;
 		}
