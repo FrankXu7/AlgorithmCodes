f920cb0f148c8148fdcd889c4d4969d7cccd6143
FrankXu7 <1248456937@qq.com>
2021-04-02 09:07:37 +0800

    添加搜索插入位置算法SearchInsertIndex

---
+++ codes/SearchInsertIndex/SearchInsertIndex.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+unsigned int SearchInsertIndex(vector<int>& nums, int target)
+
+int main()
+{
+	unsigned int insertIdx = -1;
+	
+
+	return searchIdx;
+}
