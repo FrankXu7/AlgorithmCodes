84253b74c679ec92cfd557e5dfe8e4aca3bd73a2
FrankXu7 <1248456937@qq.com>
2021-04-07 09:19:55 +0800

    完善外观数列算法

--- codes/ExteriorArray/ExteriorArray.cpp
+++ codes/ExteriorArray/ExteriorArray.cpp
@@ -1,15 +1,34 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
 string ExteriorArray(string& exteriorStr, int& n)
 {
-	int resultStr = "";
-
+	vector<string> resultStr;
+	unordered_map<char, int> tempMap;
+	tempMap.insert({ exteriorStr[0], 1 });
 	
+	char curKey = exteriorStr[0];
+	for (int idx = 1; idx < exteriorStr.length(); ++idx)
+	{
+		if (curKey == exteriorStr[idx])
+		{
+			++tempMap[curKey];
+		}
+		else
+		{
+			resultStr.push_back(tempMap[curKey] + curKey);
+			tempMap[curKey] = 0;
+			curKey = exteriorStr[idx];
+			tempMap[curKey] = 1;
 
-	return resultZtr;
+		}
+	}
+
+	return resultStr[n];
 }
 
 int main()
