fd72d8d3aa3c78c441bbc5cf05bc2fc51c7725eb
FrankXu7 <1248456937@qq.com>
2021-04-09 10:42:03 +0800

    完善最后一个单词长度和数组加一算法

--- README.md
+++ README.md
@@ -18,6 +18,8 @@
 11. [简单 - 35th] [搜索插入位置(SearchInsertIndex)](https://leetcode-cn.com/problems/search-insert-position/)
 12. [简单 - 38th] [外观数列查询(ExteriorArray)](https://leetcode-cn.com/problems/count-and-say/)
 13. [简单 - 53th] [最大子数列之和(MaxSubArraySum)](https://leetcode-cn.com/problems/maximum-subarray/)
+14. [简单 - 58th] [最后一个单词长度(LastWorldLength)](https://leetcode-cn.com/problems/length-of-last-word/)
+15. [简单 - 66th] [数组加一(ArrayPlusOne)](https://leetcode-cn.com/problems/plus-one/)
 
 
 
--- codes/ArrayPlusOne/ArrayPlusOne.cpp
+++ codes/ArrayPlusOne/ArrayPlusOne.cpp
@@ -1,3 +1,20 @@
+/**************************************************************************************************
+ * 【题目描述】
+ * 给定一个非负整数数组，数组元素为单个数字，所有元素组成一个非负整数，
+ * 给这个整数加1，并输出这个数组代表的整数。
+ *
+ * 【输入】
+ * digits=[1, 6, 8, 9]
+ * 【输出】
+ * num=1690
+ *
+ * 【解题思路】
+ * 反序遍历数组，元素加1后判断是否超过10，若超过则标记需要进位，若不超过，表示加法完成，直接退出循环。
+ * 遍历开始时，默认标记需要进位。
+ *
+ * @author FrankX
+ * @date 2021-04-09
+ **************************************************************************************************/
 #include <iostream>
 #include <vector>
 
@@ -5,26 +22,21 @@ using namespace std;
  
 vector<unsigned int> ArrayPlusOne(vector<unsigned int>& digits)
 {
-	unsigned int resultNum = 0;
-	unsigned int addOne = 1;
-
-	for (vector<unsigned int>::iterator itr = digits.end(); itr != digits.begin(); --itr)
+	if (digits.size() == 0)
 	{
-		if (addOne == 1)
-		{
-			if (*itr + 1 < 10)
-			{
-				//++(*itr);
-				*itr = *itr + 1;
-				addOne = 0;
-			}
-			else
-			{
-				*itr = 0;
-			}
-		}
-		else 
-			break;
+		digits.push_back(0);
+		return digits;
+	}
+
+	unsigned int resultNum = 0;
+	unsigned int carryOne = 1;
+
+	for (vector<unsigned int>::reverse_iterator itr = digits.rbegin(); itr != digits.rend(); ++itr)
+	{
+		if (carryOne == 0) break;
+
+		// ++ 运算符优先级高于 * 
+		(*itr + 1 < 10) ? (++(*itr), carryOne = 0) : (*itr = 0);
 	}
 
 	return digits;
@@ -32,11 +44,11 @@ vector<unsigned int> ArrayPlusOne(vector<unsigned int>& digits)
 
 int main()
 {
-	vector<unsigned int> nums = {1, 4, 7, 8};
+	vector<unsigned int> digits = {};
 
-	ArrayPlusOne(nums);
+	ArrayPlusOne(digits);
 
-	for (vector<unsigned int>::iterator itr = nums.begin(); itr != nums.end(); ++itr)
+	for (vector<unsigned int>::iterator itr = digits.begin(); itr != digits.end(); ++itr)
 	{
 		cout << *itr;
 	}
