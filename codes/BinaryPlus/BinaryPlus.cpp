bc0368333ac27f882598edffef1501825c98acdd
FrankXu7 <1248456937@qq.com>
2021-04-10 13:19:45 +0800

    二进制之和算法 BinaryPlus

--- codes/BinaryPlus/BinaryPlus.cpp
+++ codes/BinaryPlus/BinaryPlus.cpp
@@ -6,10 +6,32 @@ using namespace std;
 string BinaryPlus(string& s1, string& s2)
 {
 	string resultStr = "";
-	char carryOne = '\0';
+	int carryOne = 0;
 
-	for (int idx1 = s1.length() - 1, idx2 = s2.length() - 1; s1.length() >= 0 && s2.length() >= 0; --idx1, --idx2)
+	int idx = 0, n = 0;
+	if (s1.length() < s2.length())
 	{
+		idx = s1.length() - 1;
+		n = s2.length();
+	}
+	else
+	{
+		idx = s2.length() - 1;
+		n = s1.length();
+	}
+
+	for (; idx >= 0; --idx)
+	{
+		if (s1[idx] == s2[idx])
+		{
+			resultStr.insert(resultStr.begin(), (carryOne == 1) ? '1' : '0');
+			carryOne = (s1[idx] != '0') ? 1 : 0;
+		}
+		else
+		{
+			resultStr.insert(resultStr.begin(), '1');
+			carryOne = 0;
+		}
 	}
 	
 	return resultStr;
@@ -17,6 +39,10 @@ string BinaryPlus(string& s1, string& s2)
 
 int main()
 {
+	string s1 = "1010";
+	string s2 = "0011";
+
+	cout << BinaryPlus(s1, s2) << endl;
 
 	return 0;
 }
