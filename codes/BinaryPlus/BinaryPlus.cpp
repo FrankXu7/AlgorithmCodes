48fe91f098e8a93f9e7b7e9dcc20db7a5148d401
FrankXu7 <1248456937@qq.com>
2021-04-10 11:11:45 +0800

    添加二进制求和算法 BinaryPlus

---
+++ codes/BinaryPlus/BinaryPlus.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string BinaryPlus(string& s1, string& s2)
+{
+	string resultStr = "";
+	char carryOne = '\0';
+
+	for (int idx1 = s1.length() - 1, idx2 = s2.length() - 1; s1.length() >= 0 && s2.length() >= 0; --idx1, --idx2)
+	{
+	}
+	
+	return resultStr;
+}
+
+int main()
+{
+
+	return 0;
+}
