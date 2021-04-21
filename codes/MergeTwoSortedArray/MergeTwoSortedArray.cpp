05f8113a9e2caf4f542a27b36c1cee9aa76b1b44
FrankXu7 <1248456937@qq.com>
2021-04-21 10:38:27 +0800

    添加合并两个有序数组算法 MergeTwoSortedArray

--- codes/MergeTwoSortedArray/MergeTwoSortedArray.cpp
+++ codes/MergeTwoSortedArray/MergeTwoSortedArray.cpp
@@ -3,21 +3,50 @@
 
 using namespace std;
 
-void MergeTwoSortedArray(vector<int>& beMergeArr, vector<int>& mergeArr)
+template<typename T>
+void MergeTwoSortedArray(vector<T>& beMergeArr, vector<T>& mergeArr)
 {
+	unsigned int beMergeSize = beMergeArr.size();
+	unsigned int mergeSize = mergeArr.size();
 
+	beMergeArr.resize(beMergeSize + mergeSize);
+	typename vector<T>::reverse_iterator itr = beMergeArr.rbegin();
+	
+	for (int i = beMergeSize - 1, k = mergeSize - 1; i >= 0 || k >= 0;)
+	{
+		if (i >= 0 && k >= 0)
+		{
+			*itr = beMergeArr[i] > mergeArr[k] ? beMergeArr[i--] : mergeArr[k--];
+		}
+		else if (i >= 0)
+		{
+			*itr = beMergeArr[i--];
+		}
+		else if (k >= 0)
+		{
+			*itr = mergeArr[k--];
+		}
+		else continue;
+
+		++itr;
+	}
+
+	vector<T>().swap(mergeArr);
 }
 
 int main()
 {
-	vector<int> arr = {6,6,8,8};
-	arr[8] = 999;
-	for (vector<int>::iterator itr = arr.begin(); itr != arr.end(); ++itr)
+	vector<int> beMergeArr = {2, 3, 3, 5, 7, 9, 11, 11, 13, 27, 55, 60};
+	vector<int> mergeArr = {5, 7, 7, 10, 45, 50, 61, 61, 89};
+	
+	cout << "Size after merge: " << beMergeArr.size() + mergeArr.size() << endl;
+	MergeTwoSortedArray(beMergeArr, mergeArr);
+	
+	for (vector<int>::iterator itr = beMergeArr.begin(); itr != beMergeArr.end(); ++itr)
 	{
-		cout << *itr << '\t';
+		cout << *itr << ' ';
 	}
 	cout << endl;
-	cout << arr.size() << endl;
-	cout << arr[8] <<endl;
+
 	return 0;
 }
