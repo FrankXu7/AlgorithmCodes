899fa65469d779479947515831e96dc45e2bbb38
FrankXu7 <1248456937@qq.com>
2021-04-30 10:32:03 +0800

    完成相同二叉树-DFS递归算法

--- codes/TheSameBinaryTree/TheSameBinaryTree_DFS_Recursion.cpp
+++ codes/TheSameBinaryTree/TheSameBinaryTree_DFS_Recursion.cpp
@@ -1,3 +1,23 @@
+/**************************************************************************************************
+ * 【题目描述】
+ * 判断两颗给定的二叉树是否相同，即结构相同，对应节点的值也相同。
+ *
+ * 【输入】
+ * 两颗二叉树
+ * 【输出】
+ * 判断两颗二叉树结构与节点对应值是否相同
+ *
+ * 【解题思路】
+ * 使用深度优先算法，采用递归来实现：
+ * （1）两棵树是空二叉树，自然是相同的；
+ * （2）若其中一颗为空二叉树，另一颗非空，则二叉树不相同；
+ * （3）都是非空二叉树但节点的值不同，则二叉树不相同；
+ * （4）递归重复（1）（2）（3）步骤，直到叶子节点为止；
+ * 注意点：左右分支没有值时，指针需要设置为nullptr，否则判断为非空，指针不初始化的话可能为任意值。
+ *
+ * @author FrankX
+ * @date 2021-04-30
+ **************************************************************************************************/
 #include <iostream>
 #include <vector>
 
@@ -24,14 +44,14 @@ bool TheSameBinaryTree_DFS_Recursion(TreeNode<int>* tree1, TreeNode<int>* tree2)
 		return true;
 	}
 	else if ((tree1 == nullptr || tree2 == nullptr) ||
-		tree1->data != tree2->data)
+			 tree1->data != tree2->data)
 	{
 		return false;
 	}
 	else
 	{
 		return (TheSameBinaryTree_DFS_Recursion(tree1->left, tree2->left) && 
-			TheSameBinaryTree_DFS_Recursion(tree1->right, tree2->right));
+				TheSameBinaryTree_DFS_Recursion(tree1->right, tree2->right));
 	}
 }
 
