97b1190cc9c21b13895009f6ad2f86f46f665bd2
FrankXu7 <1248456937@qq.com>
2021-05-08 16:21:14 +0800

    修复代码错误

--- codes/TheSameBinaryTree/TheSameBinaryTree_BFS.cpp
+++ codes/TheSameBinaryTree/TheSameBinaryTree_BFS.cpp
@@ -42,15 +42,12 @@ bool TheSameBinaryTree_BFS(TreeNode<int>* tree1, TreeNode<int>* tree2)
 	queue<TreeNode<int>*> que1({ tree1 });
 	queue<TreeNode<int>*> que2({ tree2 });
 
-	while (!que1.empty() && !que2.empty())
+	while (!que1.empty() || !que2.empty())
 	{
 		if (!que1.empty()) que1.pop();
 		if (!que2.empty()) que2.pop();
 
-		if (tree1 == nullptr && tree2 == nullptr)
-		{
-			break;
-		}
+		if (tree1 == nullptr && tree2 == nullptr);
 		else if ((tree1 == nullptr || tree2 == nullptr) ||
 				 tree1->data != tree2->data)
 		{
@@ -61,12 +58,14 @@ bool TheSameBinaryTree_BFS(TreeNode<int>* tree1, TreeNode<int>* tree2)
 		 * 这里需要注意，只有左右子节点都不为空时，才需要将子节点入队；
 		 * 子节点都为空，表示已经遍历到该分支的叶子节点，不需要继续遍历了；
 		 */
-		if (tree1->left != nullptr || tree1->right != nullptr)
+		if (tree1 != nullptr &&
+			(tree1->left != nullptr || tree1->right != nullptr))
 		{
 			que1.push(tree1->left);
 			que1.push(tree1->right);
 		}
-		if (tree2->left != nullptr || tree2->right != nullptr)
+		if (tree2 != nullptr &&
+			(tree2->left != nullptr || tree2->right != nullptr))
 		{
 			que2.push(tree2->left);
 			que2.push(tree2->right);
@@ -85,19 +84,19 @@ int main()
 	TreeNode<int>* tree1 = new TreeNode<int>(4);
 	tree1->left = new TreeNode<int>(2);
 	tree1->left->left = new TreeNode<int>(1);
-	tree1->left->right = new TreeNode<int>(3);
+	tree1->left->right = new TreeNode<int>(7);
 	tree1->right = new TreeNode<int>(6);
 	tree1->right->left = new TreeNode<int>(5);
 	tree1->right->right = new TreeNode<int>(7);
-	tree1->right->right->left = new TreeNode<int>(9);
+	tree1->right->right->left = new TreeNode<int>(9356);
 	TreeNode<int>* tree2 = new TreeNode<int>(4);
 	tree2->left = new TreeNode<int>(2);
 	tree2->left->left = new TreeNode<int>(1);
-	tree2->left->right = new TreeNode<int>(3);
+	tree2->left->right = new TreeNode<int>(7);
 	tree2->right = new TreeNode<int>(6);
 	tree2->right->left = new TreeNode<int>(5);
 	tree2->right->right = new TreeNode<int>(7);
-	tree2->right->right->right = new TreeNode<int>(9);
+	tree2->right->right->right = new TreeNode<int>(9356);
 
 	cout << (TheSameBinaryTree_BFS(tree1, tree2) ? "TRUE" : "FALSE") << endl;
 
