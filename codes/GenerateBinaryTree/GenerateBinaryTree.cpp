560dbeab087c7766d46fbc3d7e8d8fe4b7977167
FrankXu7 <1248456937@qq.com>
2021-05-18 12:45:54 +0800

    有序数组生成平衡二叉树，开发代码同步

--- codes/GenerateBinaryTree/GenerateBinaryTree.cpp
+++ codes/GenerateBinaryTree/GenerateBinaryTree.cpp
@@ -5,19 +5,22 @@
 
 using namespace std;
 
-template <tepyname T>
+template <typename T>
 struct TreeNode
 {
 	T data;
+	TreeNode<T>* dad;
 	TreeNode<T>* left;
 	TreeNode<T>* right;
 
 	TreeNode<T>(T _data)
 	{
 		data = _data;
-		left = right = nullptr;
+		dad = left = right = nullptr;
 	}
 };
+
+#define NODE(Value) (new TreeNode<int>(Value))
 
 void GenerateBinaryTree(vector<TreeNode<int>*>&& treeData, TreeNode<int>*& treeRoot)
 {
@@ -26,20 +29,71 @@ void GenerateBinaryTree(vector<TreeNode<int>*>&& treeData, TreeNode<int>*& treeR
 
 void DeleteTree(TreeNode<int>*& treeRoot)
 {
-	queue<TreeNode<int>*> delQue({ treeRoot });
-	TreeNode<int>* node = nullptr;
-	
+	if (treeRoot == nullptr) return;
 
+	queue<TreeNode<int>*> delQue({ treeRoot });
+	
+	while (!delQue.empty())
+	{
+		treeRoot = delQue.front();
+		delQue.pop();
+		if (treeRoot->left) delQue.push(treeRoot->left);
+		if (treeRoot->right) delQue.push(treeRoot->right);
+
+		delete treeRoot;
+	}
 
 	treeRoot = nullptr;
 }
 
-void PrintTree()
+void PrintTree(const TreeNode<int>* treeRoot)
 {
+	if (treeRoot == nullptr)
+	{
+		cout << "Empty tree\n";
+		return;
+	}
 
+	queue<const TreeNode<int>*> printQue({ treeRoot });
+	queue<const TreeNode<int>*> tempQue;
+
+	while (!printQue.empty())
+	{
+		while (!printQue.empty())
+		{
+			treeRoot = printQue.front();
+			printQue.pop();
+			if (treeRoot) cout << treeRoot->data << '(' << treeRoot->dad->data << "), ";
+			else cout << "null, ";
+
+			if (treeRoot)
+			{
+				tempQue.push(treeRoot->left);
+				tempQue.push(treeRoot);
+			}
+		}
+		cout << endl;
+		printQue.swap(tempQue);
+	}
 }
 
 int main()
 {
+	TreeNode<int>* root = NODE(11);
+	root->right->dad = root->left->dad = root;
+
+	root->left = NODE(22);
+	root->left->left = NODE(44);
+	root->left->left->dad = root->left;
+	root->left->right = NODE(55);
+	root->left->right->dad = root->left;
+	
+
+	root->right = NODE(33);
+	root->right->left = NODE(66);
+	root->right->left->dad = root->right;
+	root->right->right = NODE(77);
+	root->right->right->dad = root->right;
+
 	return 0;
 }
