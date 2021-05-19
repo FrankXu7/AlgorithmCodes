0fa6ac3bf08194ceef7e57ba6b5c3e8e7e0af112
FrankXu7 <1248456937@qq.com>
2021-05-19 09:17:42 +0800

    有序数组生成平衡二叉树，代码同步...

--- codes/GenerateBinaryTree/GenerateBinaryTree.cpp
+++ codes/GenerateBinaryTree/GenerateBinaryTree.cpp
@@ -22,7 +22,7 @@ struct TreeNode
 
 #define NODE(Value) (new TreeNode<int>(Value))
 
-void GenerateBinaryTree(vector<TreeNode<int>*>&& treeData, TreeNode<int>*& treeRoot)
+TreeNode<int>* GenerateBinaryTree(vector<TreeNode<int>*>& treeData, int leftIdx, int rightIdx)
 {
 
 }
@@ -48,52 +48,11 @@ void DeleteTree(TreeNode<int>*& treeRoot)
 
 void PrintTree(const TreeNode<int>* treeRoot)
 {
-	if (treeRoot == nullptr)
-	{
-		cout << "Empty tree\n";
-		return;
-	}
 
-	queue<const TreeNode<int>*> printQue({ treeRoot });
-	queue<const TreeNode<int>*> tempQue;
-
-	while (!printQue.empty())
-	{
-		while (!printQue.empty())
-		{
-			treeRoot = printQue.front();
-			printQue.pop();
-			if (treeRoot) cout << treeRoot->data << '(' << treeRoot->dad->data << "), ";
-			else cout << "null, ";
-
-			if (treeRoot)
-			{
-				tempQue.push(treeRoot->left);
-				tempQue.push(treeRoot);
-			}
-		}
-		cout << endl;
-		printQue.swap(tempQue);
-	}
 }
 
 int main()
 {
-	TreeNode<int>* root = NODE(11);
-	root->right->dad = root->left->dad = root;
-
-	root->left = NODE(22);
-	root->left->left = NODE(44);
-	root->left->left->dad = root->left;
-	root->left->right = NODE(55);
-	root->left->right->dad = root->left;
-	
-
-	root->right = NODE(33);
-	root->right->left = NODE(66);
-	root->right->left->dad = root->right;
-	root->right->right = NODE(77);
-	root->right->right->dad = root->right;
 
 	return 0;
 }
