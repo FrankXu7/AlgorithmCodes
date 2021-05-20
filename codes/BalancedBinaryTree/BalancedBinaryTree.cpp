1a36c67d903dc6a533857311083cb33d39dcac34
FrankXu7 <1248456937@qq.com>
2021-05-20 13:16:44 +0800

    平衡二叉树判断，代码同步

--- codes/BalancedBinaryTree/BalancedBinaryTree.cpp
+++ codes/BalancedBinaryTree/BalancedBinaryTree.cpp
@@ -1,25 +1,18 @@
 /**************************************************************************************************
  * 【题目描述】
- * 删除一个升序列表中的重复元素。
+ * 
  *
  * 【输入】
- * list=[1,2,3,3,3,4,5]
+ * 
  *
  * 【输出】
- * newList=[1,2,3,4,5]
+ * 
  *
  * 【解题思路】
- * 因为链表是升序的，所以有重复的元素必定相邻。
- * 如果当前遍历节点的数据，等于下一个节点的数据，则当前节点的next，保存为下一个节点的next；
- * 即将下一个被删除的节点移除在链表关系之外。
- *
- * 需要注意的是：
- * （1）释放删除节点的内存，原则为同函数申请和释放，传入指针引用指向删除节点组合而成的链表。
- * （2）删除节点组合而成的链表，最后一个节点一定要置空，否则可能删除结果链表中的节点。
  *
  *
  * @author FrankX
- * @date 2021-04-16
+ * @date 2021-
  **************************************************************************************************/
 #include <iostream>
 #include <queue>
@@ -41,13 +34,45 @@ struct TreeNode
 	}
 };
 
-void DeleteTree(TreeNode<int>*& treeRoot)
+void CreateTree(vector<TreeNode<int>*>&& treeData, TreeNode<int>*& treeRoot)
 {
 
 }
 
-void PrintTree() {
+void DeleteTree(TreeNode<int>*& treeRoot)
+{
+	
+}
 
+void PrintTree(const TreeNode<int>* treeRoot) 
+{
+	queue<const TreeNode<int>*>	printQue({ treeRoot });
+	queue<const TreeNode<int>*> tempQue;
+	
+	while (!printQue.empty())
+	{
+		while (!printQue.empty())
+		{
+			treeRoot = printQue.front();
+			printQue.pop();
+
+			if (treeRoot)
+			{
+				cout << treeRoot->data;
+				if (treeRoot->dad)
+					cout << '(' << treeRoot->dad->data << "), ";
+				else 
+					cout << "(null), ";
+			}
+
+			if (treeRoot->left)
+				tempQue.push(treeRoot->left);
+			if (treeRoot->right)
+				tempQue.push(treeRoot->right);
+		}
+		cout << endl;
+		printQue.swap(tempQue);
+	}
 }
 
 int main()
