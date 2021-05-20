aaf66e42bbfc4c399be7b026d49b950998f1a645
FrankXu7 <1248456937@qq.com>
2021-05-20 08:57:16 +0800

    添加平衡二叉树判断算法

---
+++ codes/BalancedBinaryTree/BalancedBinaryTree.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+template <typename T>
+struct TreeNode
+{
+	T data;
+	TreeNode<T>* dad;
+	TreeNode<T>* left;
+	TreeNode<T>* right;
+
+	TreeNode<T>(T _data)
+	{
+		data = _data;
+		dad = left = right = nullptr;
+	}
+};
+
+void DeleteTree(TreeNode<int>*& treeRoot)
+{
+
+}
+
+void PrintTree()
+
+int main()
+{
+	return 0;
+}
