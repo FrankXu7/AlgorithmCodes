e2f590db1e2e6aa3ff4bc1e750a94ed963bec12d
FrankXu7 <1248456937@qq.com>
2021-05-21 10:39:17 +0800

    完成二叉树平衡性判断算法。

--- README.md
+++ README.md
@@ -29,6 +29,7 @@
 22. [简单 - 101th] [对称的二叉树(TheSymmetricBinaryTree)](https://leetcode-cn.com/problems/symmetric-tree/)
 23. [简单 - 104th] [二叉树深度(TheBinaryTreeDepth)](https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/)
 24. [简单 - 108th] [将有序数组转化为平衡二叉树(GenerateBinaryTree)](https://leetcode-cn.com/problems/convert-sorted-array-to-binary-search-tree/)
+25. [简单 - 110th] [判断是否为平衡二叉树(BalancedBinaryTree)](https://leetcode-cn.com/problems/balanced-binary-tree/)
 
 
 
--- codes/BalancedBinaryTree/BalancedBinaryTree.cpp
+++ codes/BalancedBinaryTree/BalancedBinaryTree.cpp
@@ -1,18 +1,34 @@
 /**************************************************************************************************
  * 【题目描述】
+ * 判断一颗给定的二叉树是否为平衡二叉树。
+ * 这里需要特别注意的是，平常大家说的平衡二叉树，即为AVL树，其必定为BST树；
+ * 但【本题中】的平衡二叉树的定义并不包含BST树。
  * 
+ * 本题应该意在二叉树的平衡判断上，不考虑是否为BST树，也算是简化了算法。
  *
  * 【输入】
- * 
+ * 一颗AVL树，不一定是BST树；
  *
  * 【输出】
- * 
+ * 是否为平衡二叉树；
  *
  * 【解题思路】
- *
+ * 方法一：
+ * 自顶向下法，即从树根节点开始，计算其左右子树最大高度差，递归计算到叶节点。
+ * 使用BFS遍历树的结点并使用队列结构，依次计算出队结点左右子树高度差，
+ * 在这之前，还需要实现给定一个树结点，返回该树最大深度的接口。
+ * 
+ * 方法二：
+ * 在方法一中，时间复杂度是 O(n^2)，且节点深度越大，节点深度重复计算的次数越多。
+ * 考虑自底向上的方法，判断结点左右子树深度高度差，其遍历的顺序符合二叉树的【后序遍历】。大致过程为：
+ * （1）从叶子结点开始，计算左右子树的深度差；
+ * （2）深度差绝对值大于1，则返回-1标识当前节点不平衡；
+ * （3）反之节点平衡，则比较左右子树深度，取更大值，即为当前节点子树最大深度，
+ *		此时注意，需要将得到的最大深度+1后返回，即为当前节点父节点子树的最大深度，
+ *		这样每个节点只计算了一次，时间复杂度将得到很大优化。
  *
  * @author FrankX 
- * @date 2021-
+ * @date 2021-05-21
  **************************************************************************************************/
 #include <iostream>
 #include <queue>
@@ -37,16 +53,54 @@ struct TreeNode
 
 #define NODE(Value) (new TreeNode<int>(Value))
 
+int GetTreeDepth(const TreeNode<int>* treeRoot);
 bool BalancedBinaryTree_1(const TreeNode<int>* treeRoot)
 {
+	queue<const TreeNode<int>*> que({ treeRoot });
+	int leftDepth = 0;
+	int rightDepth = 0;
+
+	while (!que.empty())
+	{
+		treeRoot = que.front();
+		que.pop();
+
+		if (treeRoot->left)
+		{
+			leftDepth = GetTreeDepth(treeRoot->left);
+			que.push(treeRoot->left);
+		}
+		else leftDepth = 0;
+
+		if (treeRoot->right)
+		{
+			rightDepth = GetTreeDepth(treeRoot->right);
+			que.push(treeRoot->right);
+		}
+		else rightDepth = 0;
 		
+		if (abs(leftDepth - rightDepth) > 1) return false;
+	}
 
 	return true;
 }
 
-bool BalancedBinaryTree_2()
+int BalancedBinaryTree_2(const TreeNode<int>* treeRoot)
 {
-	return true;
+	if (!treeRoot) return 0;
+
+	int leftDepth = BalancedBinaryTree_2(treeRoot->left);
+	int rightDepth = BalancedBinaryTree_2(treeRoot->right);
+
+	if (leftDepth == -1 || rightDepth == -1 || abs(leftDepth - rightDepth) > 1)
+	{
+		return -1;
+	}
+	else
+	{
+		// 返回当前节点的子树最大深度，+1是当前节点的父节点的子树最大深度
+		return (max(leftDepth, rightDepth) + 1);
+	}
 }
 
 /**
@@ -54,7 +108,7 @@ bool BalancedBinaryTree_2()
  * @param treeData 二叉树构建数据，右值引用
  * @param treeRoot 作为最终生成的树的根节点
  */
-void CreateTree(vector<TreeNode<int>*>&& treeData, TreeNode<int>*& treeRoot)
+void CreateTree(vector<TreeNode<int>*>& treeData, TreeNode<int>*& treeRoot)
 {
 	treeRoot = treeData[0];
 	TreeNode<int>* node = nullptr;
@@ -84,8 +138,10 @@ void CreateTree(vector<TreeNode<int>*>&& treeData, TreeNode<int>*& treeRoot)
  * @return 返回深度
  */
 int GetTreeDepth(const TreeNode<int>* treeRoot) {
+	if (!treeRoot) return 0;
+
 	queue<const TreeNode<int>*> tempQue({ treeRoot });
-	int depth = 1;
+	int depth = 0;
 	// BFS遍历，每一层的元素统计，默认根节点为第一层，且只有一个节点 
 	unsigned int nodeCount = tempQue.size();
 
@@ -140,10 +196,11 @@ void PrintTree(const TreeNode<int>* treeRoot)
 {
 	queue<const TreeNode<int>*>	printQue({ treeRoot });
 	queue<const TreeNode<int>*> tempQue;
+	unsigned int layerCount = 0;
 	
 	while (!printQue.empty())
 	{
-		cout << "Layer: ";
+		cout << '[' << ++layerCount << "]  ";
 		while (!printQue.empty())
 		{
 			treeRoot = printQue.front();
@@ -170,23 +227,31 @@ void PrintTree(const TreeNode<int>* treeRoot)
 
 int main()
 {
-	chrono::system_clock::time_point start = chrono::system_clock::now();
-
 	TreeNode<int>* root = nullptr;
-	CreateTree({
-		NODE(1), NODE(2), NODE(3), NODE(4), NODE(5),
-		NODE(6), NODE(7), NODE(8), NODE(9), NODE(10),
-		}, root);
-	PrintTree(root);
 
-	cout << (BalancedBinaryTree_1() ? "\nIt's a AVL tree!\n" : "\nNot AVL Tree!\n");
-	cout << (BalancedBinaryTree_2() ? "\nIt's a AVL tree!\n" : "\nNot AVL Tree!\n");
+	chrono::system_clock::time_point start = chrono::system_clock::now();
+	vector<TreeNode<int>*> treeData;
+	for (int idx = 0; idx < 10000; ++idx) treeData.push_back(NODE(idx + 1));
+	CreateTree(treeData, root);
+	chrono::system_clock::time_point end = chrono::system_clock::now();
+	cout << "[Create milliseconds]  "
+		<< chrono::duration_cast<chrono::milliseconds>(end - start).count() << endl << endl;
+
+	//PrintTree(root);
+
+	start = end;
+	cout << (BalancedBinaryTree_1(root) ? "\nIt's a AVL tree!\n" : "\nNot AVL Tree!\n");
+	end = chrono::system_clock::now();
+	cout << "[Solution_1 exec milliseconds]  " 
+		<< chrono::duration_cast<chrono::milliseconds>(end - start).count() << endl;
+
+	start = end;
+	cout << (BalancedBinaryTree_2(root) >= 0 ? "\nIt's a AVL tree!\n" : "\nNot AVL Tree!\n");
+	cout << "[Solution_2 exec milliseconds]  " 
+		<< chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now() - start).count() << endl;
 
 	DeleteTree(root);
 	
-	cout << "\nExecute Time(milliseconds): " 
-		<< chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now() - start).count()
-		<< endl;
-
+	cout << endl;
 	return 0;
 }
