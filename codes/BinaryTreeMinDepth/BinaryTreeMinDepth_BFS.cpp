b9ad0037c3690ee6235a4bced3da4a939d3f547a
FrankXu7 <1248456937@qq.com>
2021-05-26 09:05:36 +0800

    优化二叉树最小深度算法，优化二叉树路径总和算法

---
+++ codes/BinaryTreeMinDepth/BinaryTreeMinDepth_BFS.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <queue>
+#include <vextor>
+using namespace std;
+
+template <typename T>
+struct TreeNode
+{
+	T data;
+	TreeNode<T>* dad;
+	TreeNode<T>* left;
+	TreeNode<T>* right;
+
+	TreeNode<T>(T _data)
+	{
+		data = _data;
+		dad = left = right = nullptr;
+	}
+};
+
+#define NODE(value) (new TreeNode<int>*(value))
+
+void CreateTree()
