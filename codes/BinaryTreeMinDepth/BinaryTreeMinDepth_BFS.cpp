51b05b9e6dc4423a0d25f65077cc99732a13d78f
FrankXu7 <1248456937@qq.com>
2021-05-26 18:46:44 +0800

    优化二叉树最小深度算法，优化二叉树路径总和算法

--- codes/BinaryTreeMinDepth/BinaryTreeMinDepth_BFS.cpp
+++ codes/BinaryTreeMinDepth/BinaryTreeMinDepth_BFS.cpp
@@ -1,6 +1,25 @@
+/**************************************************************************************************
+ * 【题目描述】
+ * 找出一颗二叉树的最小深度。
+ *
+ * 【输入】
+ * 一颗二叉树，不一定是AVL或BST树
+ * 【输出】
+ * 该二叉树的最小深度
+ *
+ * 【解题思路】
+ * 最小深度即为根节点到最近的叶子节点的最小路径。
+ * 考虑采用自底向上的方式计算深度，遍历为后续遍历，大致过程为：
+ * （1）后续遍历，从叶子节点往上遍历到根节点，叶子节点返回深度为0；
+ * （2）当前节点左右子树的最小深度，在+1后，即为当前节点父节点的子树最小深度；
+ * （3）每层递归返回的都是子树的最小深度，到根节点时，即为二叉树的最小深度；
+ *
+ * @author FrankX
+ * @date 2021-05-22
+ **************************************************************************************************/
 #include <iostream>
 #include <queue>
-#include <vextor>
+#include <vector>
 using namespace std;
 
 template <typename T>
@@ -18,6 +37,42 @@ struct TreeNode
 	}
 };
 
-#define NODE(value) (new TreeNode<int>*(value))
+#define NODE(value) (new TreeNode<int>(value))
 
-void CreateTree()
+/**
+ * @brief 创建一颗二叉树
+ * @param treeData 二叉树结点数据容器，右值引用
+ * @param treeRoot 树根结点，指针引用
+ */
+void CreateTree(vector<TreeNode<int>*>&& treeData, TreeNode<int>*& treeRoot)
+{
+	// 空容器或首节点为空，当作空树处理，
+	if (treeData.empty() || !treeData[0])
+	{
+		treeRoot = nullptr;
+		return;
+	}
+}
+
+/**
+ * @brief 释放二叉树内存
+ * @param treeRoot 树根节点，指针引用，释放后置nullptr
+ */
+void DeleteTree(TreeNode<int>*& treeRoot)
+{
+
+}
+
+/**
+ * @brief 按BFS遍历打印一颗二叉树
+ * @param treeRoot 树根节点，常量指针，不可更改指向的数据
+ */
+void PrintTree(const TreeNode<int>* treeRoot)
+{
+
+}
+
+int main()
+{
+	return 0;
+}
