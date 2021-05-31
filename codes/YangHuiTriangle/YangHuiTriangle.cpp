0e9cb94a6f08422422c5222ef5ae9267fb620aef
FrankXu7 <1248456937@qq.com>
2021-05-31 19:44:12 +0800

    杨辉三角算法 代码同步

--- codes/YangHuiTriangle/YangHuiTriangle.cpp
+++ codes/YangHuiTriangle/YangHuiTriangle.cpp
@@ -2,19 +2,27 @@
 #include <vector>
 using namespace std;
 
-void YangHuiTriangle(const vector<vector<unsigned int>>& triangle, unsigned int numRows)
+void YangHuiTriangle(unsigned int numRows)
 {
-	triangle.push_back({ 1 });
+	vector<vector<unsigned int>> triangle = {{ 1 }};
+	cout << triangle[0][0] << endl;
+	
 	for (unsigned int row = 1; row < numRows; ++row)
 	{
+		triangle.push_back({});
+		cout << endl;
 		for (unsigned int idx = 0; idx <= row; ++idx)
 		{
-			triangle;
+			unsigned int leftNum = (idx == 0) ? 0 : triangle[row - 1][idx - 1];
+			unsigned int rightNum = (idx == triangle[row - 1].size()) ? 0 : triangle[row - 1][idx - 1];
+			cout << (leftNum + rightNum) << ", ";
+			triangle[row].push_back(leftNum + rightNum);
 		}
 	}
 }
 
 int main()
 {
+	YangHuiTriangle(10);
 	return 0;
 }
