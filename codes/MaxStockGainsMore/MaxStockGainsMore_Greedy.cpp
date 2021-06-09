a83d922148b384f73a8678204bae7134848601a3
FrankXu7 <1248456937@qq.com>
2021-06-09 09:30:59 +0800

    优化股票最大收益算法

--- codes/MaxStockGainsMore/MaxStockGainsMore_Greedy.cpp
+++ codes/MaxStockGainsMore/MaxStockGainsMore_Greedy.cpp
@@ -13,18 +13,23 @@
  * maxGains= (prices[2]-prices[1]) + (prices[4]-prices[3]) = 4 + 3 = 7
  *
  * 【解题思路】
- * 记录一个买入的价格，初始化为第一天的价格，只要遍历到的价格减去记录的买入价格为正，则卖出股票。
- * 即，只要有收益，就交易。
- * 一次交易完成后，继续下一次收益为正的交易，统计所有交易之和，即为最大收益。大致步骤：
- * （1）记录买入价格，遍历到的价格减去记录的买入价格为正，则卖出，交易完成；
- * （2）将（1）中卖出的价格记录为新的买入价格，继续遍历，只要收益为正即卖出，知道遍历完成。
+ * 从贪心角度考虑：有收益，就交易。
+ * 贪心，即只从局部考虑，找到局部最优解，汇总起来，即可看作全局的最优解。
+ * 
+ * 大致步骤：
+ * （1）假设在第i-1天买入，然后在第i天卖出，如果收益大于零，则交易（局部最优）。
+ * （2）遍历整个价格数组，相当于按遍历顺序从头到尾两两元素作为局部，
+ *		考虑它们的最优解，即局部最大收益（收益为正）。
+ *		统计所有局部最大收益，即可认为是全局的最大收益。
+ * 
+ * 需要特别注意的是，贪心算法只是计算最大利润，并不是实际交易过程。
  * 
  * 【解题分析】
  * 时间复杂度：O(n)
  * 空间复杂度：O(1)
  *
  * @author FrankX
- * @date 2021-06-08
+ * @date 2021-06-09
  **************************************************************************************************/
 #include <iostream>
 #include <vector>
@@ -35,26 +40,17 @@ int MaxStockGainsMore(vector<int>& prices)
 	if (prices.empty()) return 0;
 
 	unsigned int dataSize = prices.size();
-	// 本次交易出售的时间下标 
-	unsigned int sellIdx = 0;
-	// 买入时的价格，为当前天之前的最低价格
-	int minPrice = prices[0];
-	// 各次交易收益之和，即为总收益，记录其中的最大值 
 	int maxGains = 0;
 
-	for (unsigned int idx = sellIdx; idx < dataSize; ++idx)
+	for (unsigned int idx = 1; idx < dataSize; ++idx)
 	{
-		if (prices[idx] < minPrice)
+		// 只要收益为正，就交易 
+		if (prices[idx] - prices[idx - 1] > 0)
 		{
-			minPrice = prices[idx];
-		}
-		else if (prices[idx] - minPrice > 0)
-		{
-			// 只要收益为正，就卖出 
-			maxGains += prices[idx] - minPrice;
+			maxGains += prices[idx] - prices[idx - 1];
 
-			// 本次交易完成，最低价重置为当前遍历到的价格 
-			minPrice = prices[idx];
+			cout << "\nBuy prices[" << idx - 1 << "]=" << prices[idx - 1]
+				<< "    Sell prices[" << idx << "]=" << prices[idx];
 		}
 	}
 
@@ -63,7 +59,7 @@ int MaxStockGainsMore(vector<int>& prices)
 
 int main()
 {
-	vector<int> prices = { 2,10,9,12 };
+	vector<int> prices = { 7,1,5,3,6,4 };
 	cout << "Stock prices:\n";
 	for (vector<int>::iterator itr = prices.begin(); itr != prices.end(); ++itr)
 		cout << *itr << ", ";
