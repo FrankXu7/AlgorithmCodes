92e6e237e8b464ad34db3fffa74bc53814eb5aee
FrankXu7 <1248456937@qq.com>
2022-08-09 14:17:08 +0800

    同步提交：在排序数组中查找元素首尾下标

--- codes/SearchSortedArrRange/SearchSortedArrRange.cpp
+++ codes/SearchSortedArrRange/SearchSortedArrRange.cpp
@@ -22,30 +22,34 @@ pair<int, int> SearchSortedArrRange_1(vector<int>& nums, int target)
 {
 	pair<int, int> range(-1, -1);
 
-	auto BinarySearch = [](vector<int>& nums, int target, bool biggerSide) ->int {
+	auto BinarySearch = [](vector<int>& nums, int target, bool smallerSide) ->int {
 		int leftIdx = 0, rightIdx = static_cast<int>(nums.size()) - 1, midIdx = -1;
-		int searchedIdx = -1;
+		int searchedIdx = static_cast<int>(nums.size());
 
-		while (leftIdx < rightIdx)
+		while (leftIdx <= rightIdx)
 		{
 			midIdx = leftIdx + static_cast<int>((rightIdx - leftIdx) / 2);
-			if (nums[midIdx] < target)
-				leftIdx = midIdx + 1;
-			else if (nums[midIdx] >= target)
+			if (smallerSide)
 			{
-				rightIdx = midIdx - 1;
-				searchedIdx = midIdx;
-
-				if (biggerSide)
-					break;
+				if (nums[midIdx] < target)
+					leftIdx = midIdx + 1;
+				else
+					rightIdx = midIdx - 1;
+			}
+			else
+			{
+				if (nums[midIdx] > target)
+					rightIdx = midIdx - 1;
+				else
+					leftIdx = midIdx + 1;
 			}
 		}
 
-		return searchedIdx;
+		return smallerSide ? leftIdx : rightIdx;
 	};
 
-	range.first = BinarySearch(nums, target, false);
-	range.second = BinarySearch(nums, target, true);
+	range.first = BinarySearch(nums, target, true);
+	range.second = BinarySearch(nums, target, false);
 
 	return range;
 }
@@ -57,7 +61,8 @@ pair<int, int> SearchSortedArrRange_2(vector<int>& nums, int target)
 	int leftIdx = 0, rightIdx = static_cast<int>(nums.size()) - 1, midIdx = -1;
 	int subIdxL = 0, subIdxR = 0, subMidIdx = -1;
 
-	while (leftIdx < rightIdx)
+	// 先利用二分查找，找到等于target的元素的下标，并以此下标为基准，对其左右两侧分别进行二分法查找
+	while (leftIdx <= rightIdx)
 	{
 		midIdx = leftIdx + static_cast<int>((rightIdx - leftIdx) / 2);
 
@@ -69,33 +74,33 @@ pair<int, int> SearchSortedArrRange_2(vector<int>& nums, int target)
 		{
 			subIdxL = leftIdx;
 			subIdxR = midIdx;
-			while (subIdxL < subIdxR)
+			// 左侧，二分查找首个等于target的元素的下标
+			while (subIdxL <= subIdxR)
 			{
 				subMidIdx = subIdxL + static_cast<int>((subIdxR - subIdxL) / 2);
 
+				// 左侧部分，当元素小于target的时候，往右侧收缩，接近数组中第一个等于target的元素
 				if (nums[subMidIdx] < target)
 					subIdxL = subMidIdx + 1;
-				else if (nums[subMidIdx] == target)
-					subIdxR = subMidIdx;
+				else 
+					subIdxR = subMidIdx - 1;
 			}
 			range.first = subIdxL;
 
 			subIdxL = midIdx;
 			subIdxR = rightIdx;
-			while (subIdxL < subIdxR)
+			// 右侧，二分查找最后一个等于target的元素的下标
+			while (subIdxL <= subIdxR)
 			{
 				subMidIdx = subIdxL + static_cast<int>((subIdxR - subIdxL) / 2);
 
+				// 右侧部分，当元素大于target的时候，往左侧收缩，接近数组中最后一个等于target的元素
 				if (nums[subMidIdx] > target)
 					subIdxR = subMidIdx - 1;
-				else if (nums[subMidIdx] == target)
-				{
-					subIdxL = subMidIdx;
-					if (subIdxL + 1 == subIdxR)
-						break;
-				}
+				else 
+					subIdxL = subMidIdx + 1;
 			}
-			range.second = subIdxL;
+			range.second = subIdxR;
 
 			break;
 		}
@@ -106,7 +111,7 @@ pair<int, int> SearchSortedArrRange_2(vector<int>& nums, int target)
 
 int main(int argc, char** argv)
 {
-	vector<int> nums = { 1,2,3,3,3,4,5,6,7,8,8,8,9,10,11,12 };
+	vector<int> nums = { 1,2,3,3,3,4,5,6,7,8,8,8,8,8,9,9,10,11,12 };
 	int target = 8;
 	cout << "The given number array:\n";
 	for (int n : nums) cout << n << ", ";
